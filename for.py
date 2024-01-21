38c0ffab054366f3e7d879cadb8fb38ca025186f
MartinBuscaldi <mbuscaldi@gmail.com>
2024-01-21 18:31:48 -0300

    Version 2.00

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-nombres = ["Ana","Maria","Juan","Pedro","Pablo"]
+nombres = ["Ana","Maria","Juan","Pedro","Pablo", "Matias","Ines"]
 
 for n in nombres:
     print (f"{n}")
---
+++ zip.py
@@ -0,0 +1,5 @@
+nombres = ["Martin","Lorenzo"]
+apellidos = ["Buscaldi","Fernandez"]
+
+for n,a in zip(nombres,apellidos):
+    print(f"{a}, {n}")
